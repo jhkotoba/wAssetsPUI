9baf29e64d72c6c80214d98be10d56b5a5b7bc59
JeHoon <JeHoon@DESKTOP-0NU9ABL>
2020-08-29 02:14:57 +0900

    wAssetsPUI 파일 추가 커밋

---
+++ src/page/index/index.js
@@ -0,0 +1,82 @@
+ //세션체크
+ wFetch.getSession().then(session => {       
+    if(session.isLogin){
+        //페이지 세팅
+        PUI.userId = session.userId;
+        wFuntion.init();
+    }else{
+        //로그인페이지 이동
+        window.location.href = "/login?rtnUrl=" + window.location.href;
+    }
+ });
+ 
+ //초기화면 세팅
+ wFuntion.init = () => {
+    
+    //쿠키체크 페이지 조회
+    if(wUtil.isCookie("wApageNm")){
+        let wApageNm = wUtil.getCookie("wApageNm");
+        if(wApageNm == "undefined"){
+            wUtil.removeCookie("wApageNm");
+            wRoute.route("MAIN");
+        }else{
+            wRoute.route(wUtil.getCookie("wApageNm"));
+        }
+    }else{
+        wRoute.route("MAIN");
+    }
+    
+    //nav 클릭이벤트
+    PUI.element.nav.addEventListener("click", event => {
+        wRoute.route(event.target.dataset.menuCd);
+        event.stopPropagation();
+    });
+    
+    //main 이벤트 - click
+    PUI.element.main.addEventListener("click", event => {
+        wUtil.runFunctionIfNotEmpty(wFuntion.click, event);
+        event.stopPropagation();
+    });
+    
+    //메뉴생성
+    wFuntion.createMenu = async () => {
+        let menuList = await wFetch.getFetch("/api/admin/getMenuCodeList?mduTpCd=ASSETS");
+        let ul, li, div, a = null;
+    
+        //1레벨 메뉴 생성
+        ul = document.createElement("ul");
+        menuList.filter(menu => menu.menuLv == 1).forEach(menu => {
+            li = document.createElement("li");
+            li.classList.add("dropdown");
+            li.dataset.menuCd = menu.menuCd;
+    
+            a = document.createElement("a");
+            a.textContent = menu.menuNm;
+    
+            div = document.createElement("div");
+            div.classList.add("dropdown-content");
+    
+            li.appendChild(a);
+            li.appendChild(div);
+            ul.appendChild(li);
+        });
+    
+        //2레벨 메뉴 생성
+        menuList.filter(menu => menu.menuLv == 2).forEach(menu => {        
+            for(element of ul.childNodes){
+                if(element.dataset.menuCd === menu.menuUprCd){
+                    a = document.createElement("a");
+                    a.dataset.menuCd = menu.menuCd;
+                    a.textContent = menu.menuNm;
+                    element.firstChild.nextSibling.appendChild(a);
+                }
+            }
+        });
+    
+        PUI.element.nav.appendChild(ul);
+    };
+    wFuntion.createMenu();
+ }
+
+
+
---
+++ src/page/ledger/management/ledgerManagement.js
@@ -0,0 +1,63 @@
+//====================== init ==========================
+//페이지 초기화
+wFuntion.init = arge => {
+    
+    wFuntion.createGrid();
+}
+
+//====================== event ==========================
+//클릭 이벤트
+wFuntion.click = event => {
+    switch(event.target.id){
+    case "srhBtn": wFuntion.srhBtnClick(event); break;
+    case "regBtn": wFuntion.regBtnClick(event); break;
+    }
+};
+
+//조회 클릭이벤트
+wFuntion.srhBtnClick = event => {  
+    
+};
+
+//등록 클릭이벤트
+wFuntion.regBtnClick = event => {
+    wRoute.route("LED_REG");
+};
+
+//====================== grid ==========================
+//그리드 생성
+wFuntion.createGrid = () => {
+    PUI.grid = new wGrid("ledgerMgt", {
+        field: [
+            {title: "종류", name:"ledMgt", width:100, align:"center"},
+            {title: "가계부명", name:"ledNm", width:264, align:"left"},
+            {title: "사용자", name:"userId", width:100, align:"center"},
+            {title: "현재금액", name:"curAmt", width:100, align:"right"},
+            {title: "한달수입", name:"monIncmAmt", width:100, align:"right"},
+            {title: "한달지출", name:"monExpsAmt", width:100, align:"right"},
+            {title: "일년수입", name:"yearIncmAmt", width:100, align:"right"},
+            {title: "일년지출", name:"yearExpsAmt", width:100, align:"right"},
+            {title: "공개여부", name:"pblcYn", width:100, align:"center"},
+            {title: "건수", name:"ledRcdCnt", width:100, align:"right"},
+            {title: "최신등록일시", name:"newRegDttm", width:100, align:"center"},
+        ],
+        option: {
+            isInstantCreate: true,
+             style: {
+                 width: 1386,
+                 height: 594
+             }
+        },
+        //TEST DATA
+        //data: [],
+        data: [
+            {ledMgt: "상세", ledNm: "테스트가계부1", userId: "jh", curAmt: 25000,
+                monIncmAmt: 5000, monExpsAmt: 2000, yearIncmAmt: 100000, yearExpsAmt: 30000, pblcYn: "Y", 
+                ledRcdCnt: 424, newRegDttm: new Date().getTime()},
+            {ledMgt: "간의", ledNm: "테스트가계부2", userId: "jh", curAmt: 5000,
+                monIncmAmt: 500, monExpsAmt: 200, yearIncmAmt: 10000, yearExpsAmt: 3000, pblcYn: "Y", 
+                ledRcdCnt: 1424, newRegDttm: new Date().getTime()}
+        ]
+    });
+};
+
