a7c502f0cce368aef94b1a0b3603564aeac9ff91
JeHoon <JeHoon@DESKTOP-0NU9ABL>
2020-11-24 22:17:20 +0900

    서버 스크립트 위치 이동

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const hostname = "localhost";
+const port = 9020;
+
+const express = require("express");
+const app = express();
+
+const init = require("./init.js");
+init.start(express, app);
+
+const route = require("./route.js");
+app.use("/", route);
+
+//404
+app.use((request, response, next) => {
+  response.status(404).send("Assets PC 404");
+});
+
+//500
+app.use((error, request, response, next) => {
+  response.status(500).status("500");
+});
+
+//서버시작
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
